1d99f64588d040856acca01890ddb3c1f045451f
daksh890 <57205074+daksh890@users.noreply.github.com>
2022-06-24 10:39:32 +0530

    second commit.

---
+++ src/Components/SmallCalender.js
@@ -0,0 +1,91 @@
+import React, {useContext, useState, useEffect} from 'react'
+import dayjs from 'dayjs';
+import { getMonth } from '../util';
+import GlobalContext from '../Context/GlobalContext';
+
+export default function SmallCalender() {
+  const [currentMonthIdx, setCurrentMonthIdx] = useState(dayjs().month());
+  const [currentMonth, setCurrentMonth] = useState(getMonth()) 
+  
+
+  useEffect(()=>{
+    setCurrentMonth(getMonth(currentMonthIdx))
+  },[currentMonthIdx])
+
+  const { monthIndex, setSmallCalenderMonth, daySelected, setDaySelected } = useContext(GlobalContext)
+  
+  useEffect(()=>{
+    setCurrentMonthIdx(monthIndex);
+  },[monthIndex])
+
+  function handlePrevMonth(){
+    setCurrentMonthIdx(currentMonthIdx-1);
+  }
+  function handleNextMonth(){
+    setCurrentMonthIdx(currentMonthIdx+1);
+  }
+
+  function getDayClass(day) {
+    const format = "DD-MM-YY";
+    const slcDay = daySelected && daySelected.format(format)
+    const nowDay = dayjs().format(format)
+    const currDay = day.format(format)
+    if(nowDay === currDay){
+        return 'bg-blue-500 rounded-full text-white'
+    }
+    else if (currDay === slcDay){
+        return "bg-blue-100 rounded-full text-blue-600 font-bold"
+    } else{
+        return "";
+    }
+
+  }
+
+
+  return (
+    <div className="mt-9">
+      <header className="flex justify-between">
+        <p className="text-gray-500 font-bold">
+            {dayjs(new Date(dayjs().year(), currentMonthIdx)).format("MMMM YYYY")}
+        </p>
+        <div>
+        <button onClick={handlePrevMonth}>
+            <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+                chevron_left
+            </span>
+        </button>
+        <button onClick={handleNextMonth}>
+            <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+                chevron_right
+            </span>
+        </button>
+        </div>
+      </header>
+      <div className="grid grid-cols-7 grid-row-6">
+        {currentMonth[0].map((day, i) => (
+            <span key={i} className="text-sm py-1 text-center">
+                {day.format('dd').charAt(0)}
+            </span>
+        ))}
+        {currentMonth.map((row,i) => (
+            <React.Fragment>
+                {row.map((day, idx) => (
+                    <button 
+                    key={idx} 
+                    onClick={()=>{
+                        setSmallCalenderMonth(currentMonthIdx);
+                        setDaySelected(day)
+                    }}
+                    className={`py-1 w-full ${getDayClass(day)}`}
+                    >
+                        <span className="text-sm">
+                            {day.format('D')}
+                        </span>
+                    </button>
+                ))}
+            </React.Fragment>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ src/Context/ContextWrapper.js
@@ -0,0 +1,101 @@
+import React, {useMemo, useEffect, useState, useReducer} from 'react'
+import GlobalContext from './GlobalContext';
+import dayjs from 'dayjs';
+
+function savedEventsReducer(state, {type, payload}) {
+  switch(type) {
+    case "push": 
+        return[...state, payload];
+    
+    case "update": 
+        return state.map(evt => evt.id === payload.id ? payload : evt)
+    case "delete": 
+        return state.filter(evt => evt.id !== payload.id)
+
+    default: 
+        throw new Error();  
+  }
+}
+
+function initEvents() { 
+  const storageEvents = localStorage.getItem('savedEvents')
+  const parsedEvents = storageEvents ? JSON.parse(storageEvents) : []
+  return parsedEvents;
+}
+
+export default function ContextWrapper(props) {
+  const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  const [smallCalenderMonth, setSmallCalenderMonth] = useState(null);
+  const [daySelected, setDaySelected] = useState(dayjs());
+  const [showEventModal, setShowEventModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [labels, setLabels] = useState([]);
+
+  const [savedEvents, dispatchCalEvent] = useReducer(savedEventsReducer, [], initEvents);
+
+  const filteredEvents = useMemo(() => {
+    return savedEvents.filter(evt => 
+      labels.filter(lbl => lbl.checked)
+      .map(lbl => lbl.label)
+      .includes(evt.label)
+      );
+  },[savedEvents, labels])
+
+
+  useEffect(()=>{
+    setLabels((prevLabels) => {
+      return [...new Set( savedEvents.map(evt => evt.label))].map(label => { 
+        const currentLabel = prevLabels.find(lbl => lbl.label === label);
+        return {
+          label, 
+          checked: currentLabel ? currentLabel.checked : true,
+        };
+      });
+    });
+  }, [savedEvents]);
+
+  useEffect(()=>{
+    localStorage.setItem('savedEvents', JSON.stringify(savedEvents));
+  }, [savedEvents])
+  
+  useEffect(()=>{
+    if(smallCalenderMonth !==null){
+      setMonthIndex(smallCalenderMonth);
+    }
+  },[smallCalenderMonth])
+
+  useEffect(()=>{
+    if(!showEventModal){
+      setSelectedEvent(null);
+    }
+  }, [showEventModal])
+
+  function updateLabel(label) {
+      setLabels(labels.map((lbl) => (lbl.label === label.label ? label : lbl))
+    );
+  }
+
+  return (
+    <GlobalContext.Provider value={{
+      monthIndex, 
+      setMonthIndex, 
+      smallCalenderMonth, 
+      setSmallCalenderMonth,
+      daySelected, 
+      setDaySelected,
+      showEventModal,
+      setShowEventModal,
+      dispatchCalEvent,
+      savedEvents,
+      selectedEvent,
+      setSelectedEvent,
+      labels,
+      setLabels,
+      updateLabel,
+      filteredEvents,
+    }}
+    >
+        {props.children}
+    </GlobalContext.Provider>
+  )
+}
